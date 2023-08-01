49ad173028f4f646dcaa3d0ebcd58cd8490e1937
Jude Sajith <65233121+SajithJude@users.noreply.github.com>
2023-08-01 20:21:24 +0530

    Update upload_books.py

--- pages/upload_books.py
+++ pages/upload_books.py
@@ -1,10 +1,9 @@
 import streamlit as st
 from llama_index import GPTSimpleVectorIndex, Document, SimpleDirectoryReader, QuestionAnswerPrompt
-import os
 import openai 
 from streamlit_chat import message as st_message
 
-# import os
+import os
 import streamlit as st
 from tempfile import NamedTemporaryFile
 import PyPDF2
